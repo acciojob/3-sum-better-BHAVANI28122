cd0a16d69fc0dd171bc7022d4b896da4bf4ea3f1
BHAVANI28122 <108610488+BHAVANI28122@users.noreply.github.com>
2023-03-15 21:20:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function threeSum(arr, target) {
 // write your code here
+  let sum =0;
+	for(let i=0;i<arr.length;i++){
+		for(let j=i+1;j<arr.length;j++){
+			for(let k=j+1;k<arr.length;k++){
+				sum= sum+arr[i]+arr[j]+arr[k];
+				if(sum == target){
+					return sum;
+				}
+			}
+		}
+	}
   
 }
 
